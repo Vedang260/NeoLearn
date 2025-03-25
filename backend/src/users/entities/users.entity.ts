959329d0a4dddf843e771f8cc45ea8b4fb8983aa
Vedang <vedang2607@gmail.com>
2025-03-25 13:57:49 +0530

    users entity is created

---
+++ backend/src/users/entities/users.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { UserRole } from '../../common/enums/roles.enum';
+
+@Entity({ name: 'user' })
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+    
+    @Column({ type: 'enum', enum: UserRole })
+    role: UserRole;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
