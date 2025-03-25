c6f82d01be20e86665ed580654ef409f65d959dd
Vedang <vedang2607@gmail.com>
2025-03-25 15:59:26 +0530

    static file config added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { join } from 'path';
+
 dotenv.config()
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
+//console.log('JWT_SECRET:', process.env.JWT_SECRET);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
@@ -12,6 +15,9 @@ async function bootstrap() {
     transform: true,
   }));
   app.enableCors();
+  
+  // serving static files from uploads directory
+  app.use('/videos', express.static(join(__dirname, '..', 'uploads/videos')));
   await app.listen(process.env.PORT ?? 8000);
   console.log('🚀Server is running on port: 8000')
 }
