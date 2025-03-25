432d4e38984007e171997ff562235c1ec70bc52c
Vedang <vedang2607@gmail.com>
2025-03-26 00:37:29 +0530

    auth slice is created

---
+++ frontend/src/redux/slices/authSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AuthResponse, User } from "../../types/auth";
+
+interface AuthState {
+    user: User | null;
+    token: string | null;
+    loading: boolean;
+}
+
+const initialState: AuthState = {
+    user: null,
+    token: localStorage.getItem("token"),
+    loading: false
+}
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+      setCredentials: (state, action: PayloadAction<AuthResponse>) => {
+        state.user = null;
+        state.token = action.payload.token;
+        localStorage.setItem("token", action.payload.token);
+        state.loading = false;
+      },
+      logout: (state) => {
+        state.user = null;
+        state.token = null;
+        localStorage.removeItem("token");
+        state.loading = false;
+      },
+      setLoading: (state, action: PayloadAction<boolean>) => {
+        state.loading = true;
+      },
+    },
+  });
+
+
+  export const { setCredentials, logout, setLoading } = authSlice.actions;
+  export default authSlice.reducer;
